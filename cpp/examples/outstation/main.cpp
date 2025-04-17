b4ce1bd4a80b3720b5a80152cbaee0ab5dc5a3a4
AniketSingh990 <103957257+AniketSingh990@users.noreply.github.com>
2025-04-17 17:16:18 +0530

    Update main.cpp

--- cpp/examples/outstation/main.cpp
+++ cpp/examples/outstation/main.cpp
@@ -7,76 +7,85 @@
 #include <opendnp3/outstation/SimpleCommandHandler.h>
 #include <opendnp3/outstation/IUpdateHandler.h>
 #include <opendnp3/LogLevels.h>
-
-#include <iostream>
+#include <boost/asio.hpp>
 #include <string>
+#include <thread>
+#include <iostream>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 using namespace opendnp3;
 using namespace openpal;
 using namespace asiopal;
 using namespace asiodnp3;
+using boost::asio::ip::tcp;
+
+shared_ptr<IOutstation> outstation_global;
 
 void ConfigureDatabase(DatabaseConfig& config)
 {
-    config.analog[0].clazz = PointClass::Class2;
-    config.analog[0].svariation = StaticAnalogVariation::Group30Var5;
-    config.analog[0].evariation = EventAnalogVariation::Group32Var7;
+    // Configure digital input points for binary data (0 or 1)
+    config.digital[0].clazz = PointClass::Class2;
+    config.digital[0].svariation = StaticBinaryVariation::Group1Var2;
+    config.digital[1] = config.digital[0]; // Another digital input, you can add more if needed
 }
 
-struct State
+vector<string> split(const string& s, char delimiter)
 {
-    uint32_t count = 0;
-    double value = 0.0;
-    bool binary = false;
-    DoubleBit dbit = DoubleBit::DETERMINED_OFF;
-};
-
-void AddUpdates(UpdateBuilder& builder, State& state, const std::string& input)
-{
-    cout << "\nBefore Updates:\n";
-    cout << "Counter: " << state.count << ", Analog: " << state.value
-         << ", Binary: " << (state.binary ? "True" : "False")
-         << ", DoubleBit: " << (state.dbit == DoubleBit::DETERMINED_ON ? "ON" : "OFF") << endl;
-
-    for (char c : input)
+    vector<string> tokens;
+    string token;
+    istringstream tokenStream(s);
+    while (getline(tokenStream, token, delimiter))
     {
-        switch (c)
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+void start_tcp_sensor_listener()
+{
+    try
+    {
+        boost::asio::io_context io_context;
+        // Listen on port 20001 for the binary data
+        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 20001));
+        cout << "[INFO] Listening for binary sensor data on port 20001..." << endl;
+        while (true)
         {
-        case 'c':
-            builder.Update(Counter(state.count), 0);
-            ++state.count;
-            break;
-        case 'a':
-            builder.Update(Analog(state.value), 0);
-            state.value += 1.0;
-            break;
-        case 'b':
-            builder.Update(Binary(state.binary), 0);
-            state.binary = !state.binary;
-            break;
-        case 'd':
-            builder.Update(DoubleBitBinary(state.dbit), 0);
-            state.dbit = (state.dbit == DoubleBit::DETERMINED_OFF) ? DoubleBit::DETERMINED_ON : DoubleBit::DETERMINED_OFF;
-            break;
-        default:
-            cerr << "Unknown input character: " << c << endl;
-            break;
+            tcp::socket socket(io_context);
+            acceptor.accept(socket);
+            char data[1024] = {0};
+            size_t length = socket.read_some(boost::asio::buffer(data));
+            string received(data, length);
+            cout << "[RECEIVED] Binary data: " << received << endl;
+            
+            if (received == "0" || received == "1")
+            {
+                UpdateBuilder builder;
+                builder.Update(Binary(received == "1"), 0); // Binary data (1 or 0)
+                outstation_global->Apply(builder.Build());
+                cout << "[UPDATED] Binary data updated in DNP3 outstation." << endl;
+            }
+            else
+            {
+                cout << "[ERROR] Invalid binary data received." << endl;
+            }
+            socket.close();
         }
     }
-
-    cout << "After Updates:\n";
-    cout << "Counter: " << state.count << ", Analog: " << state.value
-         << ", Binary: " << (state.binary ? "True" : "False")
-         << ", DoubleBit: " << (state.dbit == DoubleBit::DETERMINED_ON ? "ON" : "OFF") << endl;
+    catch (exception& e)
+    {
+        cerr << "[ERROR] TCP Server: " << e.what() << endl;
+    }
 }
 
 int main(int argc, char* argv[])
 {
     const uint32_t FILTERS = levels::NORMAL | levels::ALL_COMMS;
-
     DNP3Manager manager(1, ConsoleLogger::Create());
 
+    // DNP3 listening on port 20000
     auto channel = manager.AddTCPServer(
         "server",
         FILTERS,
@@ -87,46 +96,26 @@ int main(int argc, char* argv[])
     );
 
     OutstationStackConfig config(DatabaseSizes::AllTypes(10));
-
     config.outstation.eventBufferConfig = EventBufferConfig::AllTypes(10);
     config.outstation.params.allowUnsolicited = true;
-
     config.link.LocalAddr = 10;
     config.link.RemoteAddr = 1;
-    config.link.KeepAliveTimeout = openpal::TimeDuration::Max();
-
+    config.link.KeepAliveTimeout = TimeDuration::Max();
     ConfigureDatabase(config.dbConfig);
 
-    auto outstation = channel->AddOutstation(
+    outstation_global = channel->AddOutstation(
         "outstation",
         SuccessCommandHandler::Create(),
         DefaultOutstationApplication::Create(),
         config
     );
 
-    outstation->Enable();
+    outstation_global->Enable();
+    cout << "[INFO] DNP3 Outstation started. Listening on 20000" << endl;
 
-    State state;
-    std::string input;
-
-    while (true)
-    {
-        cout << "\nEnter one or more measurement updates:\n";
-        cout << "c = counter, b = binary, d = doublebit, a = analog, 'quit' = exit\n> ";
-        cin >> input;
-
-        if (input == "quit") break;
-
-        UpdateBuilder builder;
-        AddUpdates(builder, state, input);
-
-        outstation->Apply(builder.Build());
-
-        cout << "Current State:\n";
-        cout << "Counter: " << state.count << ", Analog: " << state.value
-             << ", Binary: " << (state.binary ? "True" : "False")
-             << ", DoubleBit: " << (state.dbit == DoubleBit::DETERMINED_ON ? "ON" : "OFF") << endl;
-    }
+    // Start TCP sensor server (on 20001)
+    thread tcp_thread(start_tcp_sensor_listener);
+    tcp_thread.join();
 
     return 0;
 }
